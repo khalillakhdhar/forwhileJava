14bdeff45ed9165db08ccdc075fa271e7c3828e6
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-05 16:52:33 +0100

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,15 +1,19 @@
 package test;
 import forwhileJava.Calcule;
+import forwhileJava.Intervale;
 public class Test {
 
 	public static void main(String[] args) {
-	Calcule c=new Calcule();
-	
-		c.setA(6);
-		c.setB(45);
-	//System.out.println("la factoriel de "+c.getA()+" est "+c.factoriel());
-		System.out.println(c.toString());
+//	Calcule c=new Calcule();
+//	
+//		c.setA(6);
+//		c.setB(45);
+//	//System.out.println("la factoriel de "+c.getA()+" est "+c.factoriel());
+//		System.out.println(c.toString());
+//		
 		
+		Intervale inter=new Intervale(4, 9);
+inter.paires();
 	}
 
 }
