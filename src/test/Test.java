607aaeee2d6b4411b2945effc1f0daef2152c49f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-05 16:08:20 +0100

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,7 +4,22 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+int n=4;
+for(int i=0;i<=n;i++)
+{
+	System.out.println("i= "+i);
+	
+}
 
+while(n>0)
+{
+System.out.println("n= "+n);
+n--;
+
+}
+		
+		
+		
 	}
 
 }
