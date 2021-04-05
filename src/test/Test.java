639af4c333f9d727be39297dba6722f91d9db766
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-05 16:41:53 +0100

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,9 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 	Calcule c=new Calcule();
 	
-		c.setA(5);
-	System.out.println("la factoriel de "+c.getA()+" est "+c.factoriel());
-		
+		c.setA(6);
+		c.setB(45);
+	//System.out.println("la factoriel de "+c.getA()+" est "+c.factoriel());
+		System.out.println(c.toString());
 		
 	}
 
