c297431b08b0a350352f23db70a85e6e285319ce
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-05 16:26:40 +0100

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,10 +1,12 @@
 package test;
-
+import forwhileJava.Calcule;
 public class Test {
 
 	public static void main(String[] args) {
+	Calcule c=new Calcule();
 	
-		
+		c.setA(5);
+	System.out.println("la factoriel de "+c.getA()+" est "+c.factoriel());
 		
 		
 	}
